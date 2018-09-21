5293216173aa010dedb530a9d2c8ae23fc71c220
ZeusLMT <lemanhtuan275@gmail.com>
2018-09-21 13:16:52 +0300

    Initial commit

---
+++ app/src/main/java/com/wanderer/journal/Timeline.kt
@@ -0,0 +1,12 @@
+package com.wanderer.journal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Timeline : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+    }
+}
