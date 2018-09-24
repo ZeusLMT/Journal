56b246b8f0ad759f42b3002d5c46122e1043d83e
ZeusLMT <lemanhtuan275@gmail.com>
2018-09-24 11:59:00 +0300

    Add settings button in app bar

--- app/src/main/java/com/wanderer/journal/Timeline.kt
+++ app/src/main/java/com/wanderer/journal/Timeline.kt
@@ -1,12 +1,19 @@
 package com.wanderer.journal
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 
 class Timeline : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return true
     }
 }
