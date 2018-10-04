dffb7f05ae5c8ce6dc9186b54c5844e0d1ef5500
ZeusLMT <lemanhtuan275@gmail.com>
2018-10-04 15:40:06 +0300

    Improve settings activity with more details

--- app/src/main/java/com/wanderer/journal/Settings/SettingsFragment.kt
+++ app/src/main/java/com/wanderer/journal/Settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.wanderer.journal.Settings
 
 import android.os.Bundle
+import android.preference.EditTextPreference
+import android.preference.ListPreference
 import android.preference.PreferenceFragment
 import com.wanderer.journal.R
 
@@ -8,5 +10,25 @@ class SettingsFragment: PreferenceFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.prefs)
+
+        val viewModePref = findPreference(getString(R.string.prefs_key_view_mode)) as ListPreference
+        val gridSpanPref = findPreference(getString(R.string.prefs_key_grid_span)) as ListPreference
+        val usernamePref = findPreference(getString(R.string.prefs_key_username)) as EditTextPreference
+
+        //check grid mode is on then enable the follow up settings
+        gridSpanPref.isEnabled = viewModePref.value == "GRID"
+
+        usernamePref.summary = usernamePref.text
+
+        viewModePref.setOnPreferenceChangeListener { _, value ->
+            gridSpanPref.isEnabled = value.toString() == "GRID"
+            return@setOnPreferenceChangeListener true
+        }
+
+
+        usernamePref.setOnPreferenceChangeListener { _, value ->
+            usernamePref.summary = value.toString()
+            return@setOnPreferenceChangeListener true
+        }
     }
 }
